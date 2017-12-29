6747c1e908b1caa6346a4144b90efa8d06377d97
zacksleo <zacksleo@gmail.com>
2017-12-29 22:01:55 +0800

    style(frontend): 图片缺少域名前缀

--- src/views/frontend.php
+++ src/views/frontend.php
@@ -6,7 +6,7 @@ use yii\web\View;
 
 /* @var $this View */
 /* @var $model Share */
-$this->registerJsFile('http://res.wx.qq.com/open/js/jweixin-1.2.0.js');
+$this->registerJsFile('https://res.wx.qq.com/open/js/jweixin-1.2.0.js');
 $app = Factory::officialAccount(Yii::$app->params['wechat.officialAccount']);
 $json = $app->jssdk->buildConfig(array('onMenuShareTimeline', 'onMenuShareAppMessage'), true);
 $js = <<<JS
@@ -14,13 +14,13 @@ wx.config($json);
 wx.onMenuShareTimeline({
     title: '$model->title', 
     link: '$model->url',
-    imgUrl: '$model->image'
+    imgUrl: window.location.origin+'/$model->image'
 });
 wx.onMenuShareAppMessage({
     title: '$model->title',
     desc: '$model->description', 
     link: '$model->url', 
-    imgUrl: '$model->image'
+    imgUrl: window.location.origin+'/$model->image'
 });
 JS;
 $this->registerJs($js, View::POS_END);
